264ba2b7b60e0251c1fd1b19da4e6751ca4f16fd
ImaLuckyMan <Ima_Lucky_Man@yahoo.com>
2020-03-10 11:28:51 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Capital_Letters_in_Given_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumberCapitals("Welcome To The Jungle"));
+        }
+
+        public static int NumberCapitals(string Capital)
+        {
+            int count=0; 
+
+            foreach(var s in Capital)
+            {
+                if (char.IsUpper(s))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
